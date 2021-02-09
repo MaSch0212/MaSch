be71813afd67a5196d0ea2e834bcb8942c7010ee
Marc Schmidt <masch212@gmail.com>
2021-02-09 19:08:55 +0100

    Add back AssemblyVersionType

--- src/Presentation/Enums.cs
+++ src/Presentation/Enums.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Diagnostics.CodeAnalysis;
+using System.Reflection;
 
 namespace MaSch.Presentation
 {
@@ -170,5 +171,59 @@ namespace MaSch.Presentation
         /// The window is maximized.
         /// </summary>
         Maximized = 2,
+    }
+
+    /// <summary>
+    /// Specifies the type of version for an assembly.
+    /// </summary>
+    public enum AssemblyVersionType
+    {
+        /// <summary>
+        /// The assembly version.
+        /// </summary>
+        AssemblyVersion,
+
+        /// <summary>
+        /// The assembly file version.
+        /// </summary>
+        AssemblyFileVersion,
+
+        /// <summary>
+        /// The assembly informational version.
+        /// </summary>
+        AssemblyInformationalVersion,
+    }
+
+    /// <summary>
+    /// Provides extensions for the <see cref="AssemblyVersionType"/> enum.
+    /// </summary>
+    [SuppressMessage("StyleCop.CSharp.DocumentationRules", "SA1649:File name should match first type name", Justification = "The file name matches the enum.")]
+    public static class AssemblyVersionTypeExtensions
+    {
+        /// <summary>
+        /// Gets the version from an <see cref="Assembly"/>.
+        /// </summary>
+        /// <param name="type">The version type to get.</param>
+        /// <param name="assembly">The assembly to get the version from.</param>
+        /// <returns>The version as <see cref="string"/> for the given <see cref="Assembly"/>.</returns>
+        public static string GetVersion(this AssemblyVersionType type, Assembly assembly)
+        {
+            return type switch
+            {
+                AssemblyVersionType.AssemblyFileVersion => assembly.GetCustomAttribute<AssemblyFileVersionAttribute>().Version,
+                AssemblyVersionType.AssemblyInformationalVersion => assembly.GetCustomAttribute<AssemblyInformationalVersionAttribute>().InformationalVersion,
+                _ => assembly.GetName().Version.ToString(),
+            };
+        }
+
+        /// <summary>
+        /// Gets the version from the entry <see cref="Assembly"/>.
+        /// </summary>
+        /// <param name="type">The version type to get.</param>
+        /// <returns>The version as <see cref="string"/> for the entry <see cref="Assembly"/>.</returns>
+        public static string GetVersion(this AssemblyVersionType type)
+        {
+            return GetVersion(type, Assembly.GetEntryAssembly());
+        }
     }
 }
