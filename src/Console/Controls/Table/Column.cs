831d15b313660cbf96a7809dbc1381d1397281e9
Marc Schmidt <masch212@gmail.com>
2021-03-28 13:29:32 +0200

    Add missing documentation

--- src/Console/Controls/Table/Column.cs
+++ src/Console/Controls/Table/Column.cs
@@ -1,24 +1,54 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace MaSch.Console.Controls.Table
+﻿namespace MaSch.Console.Controls.Table
 {
+    /// <summary>
+    /// Represents a coplumn of a <see cref="TableControl"/>.
+    /// </summary>
     public class Column
     {
+        /// <summary>
+        /// Gets or sets the header.
+        /// </summary>
         public string? Header { get; set; }
+
+        /// <summary>
+        /// Gets or sets the width.
+        /// </summary>
         public double Width { get; set; } = double.NaN;
+
+        /// <summary>
+        /// Gets or sets the width mode.
+        /// </summary>
         public ColumnWidthMode WidthMode { get; set; } = ColumnWidthMode.Auto;
+
+        /// <summary>
+        /// Gets or sets the minimum width.
+        /// </summary>
         public int? MinWidth { get; set; } = null;
+
+        /// <summary>
+        /// Gets or sets the maximum width.
+        /// </summary>
         public int? MaxWidth { get; set; } = null;
     }
 
+    /// <summary>
+    /// Width modes that can be used for <see cref="Column"/>s.
+    /// </summary>
     public enum ColumnWidthMode
     {
+        /// <summary>
+        /// The width is automatically determined by the content of the <see cref="Column"/>.
+        /// </summary>
         Auto,
+
+        /// <summary>
+        /// The width is determined by the fixed <see cref="Column.Width"/> of the <see cref="Column"/>.
+        /// </summary>
         Fixed,
+
+        /// <summary>
+        /// The width is determined by an amount of the available with of the <see cref="TableControl"/>.
+        /// </summary>
         Star,
     }
 }
--- src/Console/Controls/Table/Row.cs
+++ src/Console/Controls/Table/Row.cs
@@ -1,22 +1,47 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace MaSch.Console.Controls.Table
 {
+    /// <summary>
+    /// Represents a row in a <see cref="TableControl"/>.
+    /// </summary>
     public class Row
     {
+        /// <summary>
+        /// Gets or sets a value indicating whether this instance is a seperator instead of a data row.
+        /// </summary>
         public bool IsSeperator { get; set; } = false;
+
+        /// <summary>
+        /// Gets or sets the values.
+        /// </summary>
         public IList<string> Values { get; set; } = Array.Empty<string>();
+
+        /// <summary>
+        /// Gets or sets the height.
+        /// </summary>
         public int? Height { get; set; } = null;
+
+        /// <summary>
+        /// Gets or sets the height mode.
+        /// </summary>
         public RowHeightMode HeightMode { get; set; } = RowHeightMode.Auto;
     }
 
+    /// <summary>
+    /// Height modes that can be used for <see cref="Row"/>s.
+    /// </summary>
     public enum RowHeightMode
     {
+        /// <summary>
+        /// The height is automatically determined by the content of the <see cref="Row"/>.
+        /// </summary>
         Auto,
+
+        /// <summary>
+        /// The height is determined by the fixed <see cref="Row.Height"/> of the <see cref="Row"/>.
+        /// </summary>
         Fixed,
     }
 }
